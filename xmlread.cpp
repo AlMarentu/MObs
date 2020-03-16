3f8deed26983a097cc7f5bd5af0d549bb075b3ca
Matthias <matthias@rentual.de>
2020-03-17 00:00:57 +0100

    Object inserter in eigene Klasse

--- xmlread.cpp
+++ xmlread.cpp
@@ -8,87 +8,36 @@
 
 using namespace std;
 
-
-
-
-class XmlReadData : public XmlParser  {
+class ObjectInserter  {
 public:
-  XmlReadData(const string &input) : XmlParser(input) {  };
-  ~XmlReadData() { };
+  ObjectInserter() { }
+  ~ObjectInserter() { }
   
-// stack<bool> inArray;
-  
-  class Objekt {
-  public:
-    Objekt(ObjectBase *o, const string &name) {
-      obj = o;
-      objName = name;
-      cerr << "OBJ " << name << endl;
-    };
-    ~Objekt() {};
-    
-    string objName;
-    ObjectBase *obj = 0;
-  };
-  string encoding;
-  stack<Objekt> objekte;
-  vector<string> path;
-  
-    
-  void NullTag(const string &element) {
-    TRACE(PARAM(element));
-    cerr << "NULL " << element << endl;
-
-    if (memBase and memBase->nullAllowed())
-    {
-      memBase->setNull(true);
-    }
-    EndTag(element);
-  };
-  void Attribut(const string &element, const string &attribut, const string &value) {
-    TRACE(PARAM(element) << PARAM(attribut)<< PARAM(value));
-    cerr << "Attribut " << element << " " << attribut << " " << value << endl;
-
-  };
-  void Value(const string &value) {
-    TRACE(PARAM(value));
-    cerr << "Value " << value << endl;
+  inline MemberBase *member() const { return memBase; };
+  string showpath() const {
     string p;
     for (auto &i:path)
       p += i + "::";
-    if (not memBase)
-      cerr << "Variable fehlt " << p << endl;
-    else
-      memBase->fromStr(value);
-  };
-  void Cdata(const char *val, size_t len) {
-    string value(val, len);
-    TRACE(PARAM(value));
-    cerr << "Cdata " << value << " atadC" << endl;
-
-  };
-  void StartTag(const string &element) {
+    return p;
+  }
+  void pushObject(ObjectBase &obj) {
+    objekte.push(ObjectInserter::Objekt(&obj, ""));
+  }
+  bool enter(const string &element) {
     TRACE(PARAM(element));
-    cerr << "Start " << element << " " << tagPath().size() <<  endl;
     memBase = 0;
     path.push_back(element);
-    string p;
-    for (auto &i:path)
-      p += i + "::";
-    //cerr << "XX " << p << endl;
     
     if (objekte.empty())
       throw runtime_error(u8"XmlRead: Fatal: keine Objekt");
-    if (tagPath().size() <= 1)
-      return;
     
     cerr << "Sind im Object " << objekte.top().objName << endl;
     ObjectBase *o = objekte.top().obj->getObjInfo(element);
     if (o)
     {
       cerr << element << " ist ein Objekt" << endl;
-      objekte.push(XmlReadData::Objekt(o, o->name()));
-      return;
+      objekte.push(ObjectInserter::Objekt(o, o->name()));
+      return true;
     }
     MemBaseVector *v = objekte.top().obj->getVecInfo(element);
     if (v)
@@ -101,43 +50,124 @@ public:
       if (o)
       {
         //          cerr << "Objekt" << endl;
-        objekte.push(XmlReadData::Objekt(o, element));
+        objekte.push(ObjectInserter::Objekt(o, element));
+        return true;
       }
       else if (m)
       {
         //          cerr << "Member" << endl;
         memBase = m;
+        return true;
       }
-      else
-        cerr << element << " ist ein Vector von WeisNichtWas" << endl;
+      objekte.push(ObjectInserter::Objekt(nullptr, element));
       
-      return;
+      return false;
     }
     MemberBase *m = objekte.top().obj->getMemInfo(element);
     if (m)
     {
       //        cerr << element << " ist ein Member" << endl;
       memBase = m;
-      return;
+      return true;
     }
     cerr << element << " ist ein WeisNichtWas" << endl;
+    return false;
+  }
+  void exit(const string &element = "") {
+    TRACE(PARAM(element));
+    if (memBase)
+      memBase = 0;
+    else if (objekte.empty())
+      throw runtime_error(u8"Objektstack underflow");
+    else
+      objekte.pop();
+    if (not element.empty() and path[path.size() -1] != element)
+      throw runtime_error(u8"exit Object expected " + path[path.size() -1] + " got " + element);
+    path.resize(path.size()-1);
+  }
+  
+  
+  
+private:
+  class Objekt {
+  public:
+    Objekt(ObjectBase *o, const string &name) {
+      obj = o;
+      objName = name;
+      cerr << "OBJ " << name << endl;
+    };
+    ~Objekt() {};
     
+    string objName;
+    ObjectBase *obj = 0;
+  };
+  stack<Objekt> objekte;
+  vector<string> path;
+  
+
+  MemberBase *memBase = 0;
+};
+
+
+
+
+class XmlReadData : public ObjectInserter, public XmlParser  {
+public:
+  XmlReadData(const string &input) : XmlParser(input) {  };
+  ~XmlReadData() { };
+  
+// stack<bool> inArray;
+  
+  string encoding;
     
+  void NullTag(const string &element) {
+    TRACE(PARAM(element));
+    cerr << "NULL " << element << endl;
+
+    if (member() and member()->nullAllowed())
+    {
+      member()->setNull(true);
+    }
+    EndTag(element);
+  };
+  void Attribut(const string &element, const string &attribut, const string &value) {
+    TRACE(PARAM(element) << PARAM(attribut)<< PARAM(value));
+    cerr << "Attribut " << element << " " << attribut << " " << value << endl;
+
+  };
+  void Value(const string &value) {
+    TRACE(PARAM(value));
+    cerr << "Value " << value << endl;
+    if (not member())
+      cerr << "Variable fehlt " << showpath() << endl;
+    else
+      member()->fromStr(value);
+  };
+  void Cdata(const char *val, size_t len) {
+    string value(val, len);
+    TRACE(PARAM(value));
+    cerr << "Cdata " << value << " atadC" << endl;
+    if (not member())
+      cerr << "Variable fehlt " << showpath() << endl;
+    else
+      member()->fromStr(value);
+  };
+  void StartTag(const string &element) {
+    TRACE(PARAM(element));
+    cerr << "Start " << element << " " << tagPath().size() <<  endl;
+
+    if (tagPath().size() <= 1)
+      return;
+    if (not enter(element))
+      cerr << element << " wurde nicht gefunden" << endl;
+
 
   }
   void EndTag(const string &element) {
     TRACE(PARAM(element));
     cerr << "Ende " << element << endl;
-    path.resize(path.size()-1);
-    if (memBase)
-    {
-      memBase = 0;
-      return;
-    }
-    if (not objekte.empty())
-      objekte.pop();
-    else if (tagPath().size() > 1)
-      throw runtime_error(u8"Objektstack underflow");
+    if (tagPath().size() > 1)
+      exit(element);
   }
   void ProcessingInstruction(const string &element, const string &attribut, const string &value) {
     TRACE(PARAM(element) << PARAM(attribut)<< PARAM(value));
@@ -150,7 +180,6 @@ public:
   
   private:
     string prefix;;
-  MemberBase *memBase = 0;
   };
   
   
@@ -176,7 +205,7 @@ public:
   void XmlRead::fill(ObjectBase &obj)
   {
     TRACE("");
-    data->objekte.push(XmlReadData::Objekt(&obj, ""));
+    data->pushObject(obj);
     try {
       parse();
     } catch (exception &e) {
