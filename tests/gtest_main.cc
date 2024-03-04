3f9325fb6250dff399e27d7655abbb5b97c2e9ea
Matthias <matthias@rentual.de>
2024-03-04 20:15:51 +0100

    Mke überarbeitet, gtest_main hinzu

--- tests/gtest_main.cc
+++ tests/gtest_main.cc
@@ -7,9 +7,9 @@ GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from %s\n", __FILE__);
 #ifdef __WIN32__
   //setlocale(LC_CTYPE, ".utf8");
-  setlocale(LC_CTYPE, "");
 #endif
-  std::cerr << "LC_ALL=" << setlocale(LC_CTYPE, 0) << '\n';
+  setlocale(LC_CTYPE, "");
+  std::cerr << "LC_ALL=" << setlocale(LC_CTYPE, nullptr) << '\n';
 
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
